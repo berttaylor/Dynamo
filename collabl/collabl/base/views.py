8320f74c281519d09055c130311675942485bc7e
Bertie Taylor <taftaylor@gmail.com>
2022-02-12 22:59:56 +0000

    styling pass 2

--- collabl/collabl/base/views.py
+++ collabl/collabl/base/views.py
@@ -1,4 +1,6 @@
 from django.http import HttpResponse
+from django.urls import reverse_lazy
+from django.views.generic import RedirectView
 
 """
 Generic views needed for front end functionality are kept here.
@@ -7,3 +9,20 @@ Generic views needed for front end functionality are kept here.
 
 def empty_string(request):
     return HttpResponse()
+
+
+class HomepageRedirectView(RedirectView):
+    """
+    This view directs the user to the correct 'home' location, depending on whether they are logged in or not
+    """
+
+    def get_redirect_url(self, *args, **kwargs):
+        user = self.request.user
+        match user.is_authenticated:
+            # if logged_in, direct to the dashboard
+            case True:
+                return reverse_lazy("user-group-list")
+
+            # if unauthenticated, redirect to agency directory
+            case _:
+                return reverse_lazy("landing")
--- collabl/users/forms.py
+++ collabl/users/forms.py
@@ -46,7 +46,7 @@ class UserDetailUpdateForm(ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         for field_name, field in self.fields.items():
-            field.widget.attrs["class"] = "form-control"
+            field.widget.attrs["class"] = "form-control btn-primary"
 
 
 class CustomLoginForm(AuthenticationForm):
