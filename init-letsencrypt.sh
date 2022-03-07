8658efb40b67711bc2031034fc6142e2586f2d8f
Bertie Taylor <taftaylor@gmail.com>
2022-03-07 19:33:13 +0100

    prod setup

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -7,7 +7,7 @@ fi
 
 domains=(collabl.io www.collabl.io)
 rsa_key_size=4096
-data_path="./data/certbot"
+data_path="./certificates/certbot"
 email="taftaylor@gmail.com" # Adding a valid address is strongly recommended
 staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
@@ -25,6 +25,11 @@ if [ ! -e "$data_path/conf/options-ssl-nginx.conf" ] || [ ! -e "$data_path/conf/
   curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot-nginx/certbot_nginx/_internal/tls_configs/options-ssl-nginx.conf > "$data_path/conf/options-ssl-nginx.conf"
   curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot/certbot/ssl-dhparams.pem > "$data_path/conf/ssl-dhparams.pem"
   echo
+fi
+
+if [ ! -e "/etc/letsencrypt/live/$domains" ]; then
+  echo "### Making folders"
+  mkdir -p "$data_path/conf/live/$domains"
 fi
 
 echo "### Creating dummy certificate for $domains ..."
